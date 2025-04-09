7393677c4bccd1b5d045a74bbc00fb4cd2b91502
Tharun0730 <tharunsankari007@gmail.com>
2025-04-09 16:32:29 +0530

    Started the project and added the components and test api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Sidebar from './components/layout/SideBAr'
-import ChatContainer from './components/chatContainerComponents/ChatContainer'
-import MainLayout from './pages/mainLayout'
 import MainRoute from './Routes/MainRoutes'
 
 
